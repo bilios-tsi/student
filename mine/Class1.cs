7a0535253ecb221c58a25f46cab735e041681782
bill <bili0stsi@gmail.com>
2020-07-19 16:58:22 +0300

    C# student

---
+++ mine/Class1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace mine
+{
+    public class Student
+    {
+        private string id;
+        public string Id
+        {
+            
+            set
+            {
+                id = value;
+            }
+            get { return id; }
+        }
+
+        private string LastName;
+        public string LN { set; get; }
+
+        private string FirstName;
+        public string FN { set; get; }
+
+        private int age;
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0 && value <= 150)
+                    age = value;
+            }
+        }
+    }
+}
---
+++ mine/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Diagnostics.Tracing;
+
+namespace mine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int c = 0;
+            string getage;
+            Student S = new Student();
+            c += 1;
+            S.Id = "CD" + c.ToString();
+            Console.WriteLine("Hello Student!");
+            Console.WriteLine("Give your Age");
+            S.Age = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("give your last name");
+            S.LN = Console.ReadLine();
+            Console.WriteLine("give your first name");
+            S.FN = Console.ReadLine();
+            Console.WriteLine(S.FN +" "+ S.LN + " is " + S.Age + " years old and your ID is "+S.Id);
+            Console.ReadKey();
+
+        }
+    }
+}
